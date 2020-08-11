8605813ab01c8079bde12a64f1c1098a41d6be5a
bartoszpankratz <32309411+bartoszpankratz@users.noreply.github.com>
2020-08-11 23:23:26 +0200

    Update VAR.py

--- VAR.py
+++ VAR.py
@@ -14,18 +14,19 @@ import numpy as np
         #}
 		
 class VaR:
-    def __init__(self, data, method, alpha, look_back):
-        self.name  = data.metadata["NAME"] + "_" + method
+    def __init__(self, data, methods, alpha, look_back):
+        self.name  = data.metadata["NAME"] + "_" + "VAR"
         self.vars = pd.DataFrame({
             'ID'    : data.data.index,
             'Returns' : data.data['Returns']})
         self.vars = self.vars.set_index('ID')
         if "True_VAR_0.01" in data.data.columns:
             self.vars['True_VAR_0.01'] = data.data["True_VAR_0.01"]
-        if method == "VCA":
-            self.calc_VCA(alpha,look_back)
-        elif method == "HIST":
-            self.calc_Hist(alpha,look_back)
+		for method in methods:
+			if method == "VCA":
+				self.calc_VCA(alpha,look_back)
+			elif method == "HIST":
+				self.calc_Hist(alpha,look_back)
             
     def calc_VCA(self,alpha,look_back):
         INV_NORM   = sps.norm.ppf(alpha)
